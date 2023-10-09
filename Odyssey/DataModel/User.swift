d36d1f32fe24eb3afe52eb081d526e5ae725d9e5
Rajwinder Singh <irajwinder@users.noreply.github.com>
2023-10-09 17:32:43 -0400

    App logic

---
+++ Odyssey/DataModel/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Odyssey
+//
+//  Created by Rajwinder Singh on 10/9/23.
+//
+
+import Foundation
+
+struct TicketInfo {
+    var location: String
+    var country: String
+    var state: String
+    var city: String
+}
